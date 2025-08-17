a45f6ef2d876456b7314f99e7c4831980440dd36
Snehal Chavan <153723087+snehalchavan96@users.noreply.github.com>
2025-08-18 00:05:09 +0530

    DiamondProbSol.java

--- InterviewProg/DiamondProbSol.java
+++ InterviewProg/DiamondProbSol.java
@@ -1,12 +1,19 @@
-public class DiamondProbSol implements I1,I2
-{	
-		public void m1()
-		{
-			System.out.println(" from m1() of class DiamondP ");
-		}
-		
-		public static void main(String[] args) {
-			DiamondProbSol p1=new DiamondProbSol();
-			p1.m1();
-		}
-	}
+interface I1 {
+    void m1();
+}
+
+interface I2 {
+    void m1();
+}
+
+public class DiamondProbSol implements I1, I2 {
+    // Only one implementation is needed (resolves diamond problem)
+    public void m1() {
+        System.out.println("from m1() of class DiamondProbSol");
+    }
+
+    public static void main(String[] args) {
+        DiamondProbSol p1 = new DiamondProbSol();
+        p1.m1(); // calls the single implementation
+    }
+}
