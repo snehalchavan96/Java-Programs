91a333ec5f8a7bc8ec5f70ccea3ffec39693672e
Snehal Chavan <153723087+snehalchavan96@users.noreply.github.com>
2025-08-18 00:03:11 +0530

    Update Arrange0_1_2.java

--- InterviewProg/Arrange0_1_2.java
+++ InterviewProg/Arrange0_1_2.java
@@ -1,18 +1,38 @@
 package InterviewProg;
 
+import java.util.Arrays;
+
 public class Arrange0_1_2 {
 
-	int arr[]= {2,0,1,2,1,0,2,2,0,0,2,1};
-	
-	
-	int high=arr.length-1;
-	int low=0;
-	int mid=0,temp;
-	
-	while(arr[mid]<=arr[high])
-	{
-		
-		
-	}
-	
+    public static void main(String[] args) {
+        int arr[] = {2, 0, 1, 2, 1, 0, 2, 2, 0, 0, 2, 1};
+
+        int low = 0, mid = 0, high = arr.length - 1;
+        int temp;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:  // Swap arr[low] and arr[mid], increment both
+                    temp = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+
+                case 1:  // Just move ahead
+                    mid++;
+                    break;
+
+                case 2:  // Swap arr[mid] and arr[high], decrement high
+                    temp = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = temp;
+                    high--;
+                    break;
+            }
+        }
+        // Print sorted array
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
 }
